bb4cc711ef765fb1947abdc38074d606ce14237f
N__Bear <meduator33@gmail.com>
2019-02-03 22:57:19 +0200

    complete site layout

--- www/js/src.js
+++ www/js/src.js
@@ -1,12 +1,8 @@
-console.log(document)
-var page = document.querySelector('.page');
-console.log(page)
-var pageWrapper = page.querySelector('.wrapper');
-console.dir(page)
-console.dir(pageWrapper)
-var h1 = page.offsetHeight
-var h2 = pageWrapper.offsetHeight
-console.log( h1, h2)
-if(h1 < h2){
-  page.classList.add('test')
-}
+var menu = document.querySelector('.main-nav');
+console.log(menu);
+var menuOpener = document.querySelector('.main-nav__opener');
+
+menuOpener.addEventListener('click', function() {
+   menu.classList.toggle('is-active');
+   menuOpener.classList.toggle('is-active');
+});
